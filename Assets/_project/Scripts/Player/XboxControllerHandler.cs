9dcbc00020294017e4edad97ae4da316616267d9
Charles Carter <cmcrter@gmail.com>
2020-10-10 15:58:51 +0000

    Setting up first joystick controls

--- Assets/_project/Scripts/Player/XboxControllerHandler.cs
+++ Assets/_project/Scripts/Player/XboxControllerHandler.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class XboxControllerHandler : MonoBehaviour
+public class XboxControllerHandler : InputHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    int playerID = 1;
+
+    public override bool MoveLeft()
     {
-        
+        return Input.GetAxis("MoveHorizontal" + playerID.ToString()) < 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool MoveRight()
     {
-        
+        return Input.GetAxis("MoveHorizontal" + playerID.ToString()) > 0;
+    }
+
+    public override bool Jump()
+    {
+        return Input.GetButtonDown("Jump" + playerID.ToString());
+    }
+
+    public override bool Crouch()
+    {
+        return Input.GetButtonDown("Crouch" + playerID.ToString());
+    }
+
+    public override bool FireWeapon()
+    {
+        return Input.GetButtonDown("Fire" + playerID.ToString());
+    }
+
+    public override bool ThrowWeapon()
+    {
+        return Input.GetButtonDown("Throw" + playerID.ToString());
+    }
+
+    public override bool Interact()
+    {
+        return Input.GetButtonDown("Interact" + playerID.ToString());
     }
 }
